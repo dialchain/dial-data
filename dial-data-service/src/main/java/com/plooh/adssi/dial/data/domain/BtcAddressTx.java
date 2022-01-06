9998d3e54b97a0f60d87baf8266263d39891b6ac
Francis Pouatcha <fpo@adorsys.de>
2022-01-06 12:42:40 -0500

    Implementing processing of mempool transactions

--- dial-data-service/src/main/java/com/plooh/adssi/dial/data/domain/BtcAddressTx.java
+++ dial-data-service/src/main/java/com/plooh/adssi/dial/data/domain/BtcAddressTx.java
@@ -1,6 +1,7 @@
 package com.plooh.adssi.dial.data.domain;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.LegacyAddress;
@@ -10,6 +11,10 @@ import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Transaction;
 
 public class BtcAddressTx {
+    public final static int VERSION_0_OUTPUT = 0;
+    public final static int VERSION_0_INPUT = 1;
+    public final static int VERSION_1_OUTPUT = 2;
+    public final static int VERSION_1_INPUT = 3;
     /// Can be used to map following version information
     /// 0 -> 00 Output fix length, no tx info
     /// 1 -> 01 Input fix length, no tx info
@@ -35,38 +40,41 @@ public class BtcAddressTx {
 
     static final int legacy_btc_addr_length = 1 + Sha256Hash.LENGTH + Sha256Hash.LENGTH + LegacyAddress.LENGTH;
     static final int legacy_btc_segwith_length = 1 + Sha256Hash.LENGTH + Sha256Hash.LENGTH
-            + SegwitAddress.WITNESS_PROGRAM_LENGTH_PKH;
+            + SegwitAddress.WITNESS_PROGRAM_MAX_LENGTH;
 
     public static BtcAddressTx fromBytes(NetworkParameters params, byte[] b) {
-        if (b.length == legacy_btc_addr_length || b.length == legacy_btc_segwith_length) {
+        if (b.length == legacy_btc_addr_length || b.length <= legacy_btc_segwith_length) {
             return fromBytesV0(params, b);
         }
 
         ByteBuffer buffer = ByteBuffer.wrap(b);
+        // Read version byte
         int version = buffer.getInt();
+
+        // Read the length of the txId byte
         int txIdLength = buffer.getInt();
         byte[] txId = null;
         if (txIdLength > 0) {
             txId = new byte[txIdLength];
             buffer.get(txId);
         }
+
         int blockIdLength = buffer.getInt();
         byte[] blockId = null;
         if (blockIdLength > 0) {
             blockId = new byte[blockIdLength];
             buffer.get(blockId);
         }
+
         int addrLength = buffer.getInt();
         Address address = null;
         if (addrLength > 0) {
             byte[] addressBytes = new byte[addrLength];
             buffer.get(addressBytes);
-            if (addressBytes.length == LegacyAddress.LENGTH) {
-                address = LegacyAddress.fromScriptHash(params, addressBytes);
-            } else if (addressBytes.length == SegwitAddress.WITNESS_PROGRAM_LENGTH_PKH) {
-                address = SegwitAddress.fromHash(params, addressBytes);
-            }
+            var addressStr = new String(addressBytes, StandardCharsets.US_ASCII);
+            address = Address.fromString(params, addressStr);
         }
+
         int txLength = buffer.getInt();
         Transaction tx = null;
         if (txLength > 0) {
@@ -74,6 +82,7 @@ public class BtcAddressTx {
             buffer.get(txBytes);
             tx = new Transaction(params, txBytes, 0);
         }
+
         return new BtcAddressTx(version, txId == null ? null : Sha256Hash.wrap(txId),
                 blockId == null ? null : Sha256Hash.wrap(blockId), address,
                 tx);
@@ -94,26 +103,35 @@ public class BtcAddressTx {
         Address address = null;
         if (addressBytes.length == LegacyAddress.LENGTH) {
             address = LegacyAddress.fromScriptHash(params, addressBytes);
-        } else if (addressBytes.length == SegwitAddress.WITNESS_PROGRAM_LENGTH_PKH) {
+        } else {
             address = SegwitAddress.fromHash(params, addressBytes);
         }
         return new BtcAddressTx(input, Sha256Hash.wrap(txId), Sha256Hash.wrap(blockId), address, null);
     }
 
-    // TODO Willyimplement V1 serialization
     public byte[] toBytes() {
-        byte[] addressBytes = address.getHash();
-        int capacity = 1 + Sha256Hash.LENGTH + Sha256Hash.LENGTH + addressBytes.length;
+        int intLength = 4;
+        int versionLength = 4;
+        int txIdLength = txId.getBytes().length;
+        int blockIdLength = blockId.getBytes().length;
+        byte[] addressBytes = address.toString().getBytes(StandardCharsets.US_ASCII);
+        int addrLength = addressBytes.length;
+        byte[] txBytes = tx == null ? new byte[0] : tx.unsafeBitcoinSerialize();
+        int txLength = txBytes.length;
+
+        int capacity = versionLength + intLength + txIdLength + intLength + blockIdLength + intLength + addrLength
+                + intLength + txLength;
         ByteBuffer buffer = ByteBuffer.allocate(capacity);
-        buffer.put(();
-        buffer.put(txId.getBytes());
-        buffer.put(blockId.getBytes());
-        buffer.put(addressBytes);
+        buffer.putInt(version)
+                .putInt(txIdLength).put(txId.getBytes())
+                .putInt(blockIdLength).put(blockId.getBytes())
+                .putInt(addrLength).put(addressBytes)
+                .putInt(txLength).put(txBytes);
         return buffer.array();
     }
 
-    public boolean isInput() {
-        return input;
+    public int getVersion() {
+        return version;
     }
 
     public Sha256Hash getTxId() {
@@ -127,45 +145,4 @@ public class BtcAddressTx {
     public Address getAddress() {
         return address;
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        result = prime * result + ((blockId == null) ? 0 : blockId.hashCode());
-        result = prime * result + (input ? 1231 : 1237);
-        result = prime * result + ((txId == null) ? 0 : txId.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        BtcAddressTx other = (BtcAddressTx) obj;
-        if (address == null) {
-            if (other.address != null)
-                return false;
-        } else if (!address.equals(other.address))
-            return false;
-        if (blockId == null) {
-            if (other.blockId != null)
-                return false;
-        } else if (!blockId.equals(other.blockId))
-            return false;
-        if (input != other.input)
-            return false;
-        if (txId == null) {
-            if (other.txId != null)
-                return false;
-        } else if (!txId.equals(other.txId))
-            return false;
-        return true;
-    }
-
 }
